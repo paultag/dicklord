a3b9991ce99ddb9590ce359b46888049cda0b9c5
Paul Tagliamonte <tag@pault.ag>
2013-12-08 08:45:19 -0500

    add more things

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import twilio
 import local_settings
 from twilio.rest import TwilioRestClient
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,8 +18,12 @@ def send_text(whom, message):
 
 @app.route('/')
 def index():
+
+    fro = request.args.get('From', None)
+    SMS = "Yo, %s is at the door." % (fro)
+
     for number in local_settings.team_numbers:
-        send_text(number, 'test')
+        send_text(number, fro)
     return render_template("root.xml", **{})
 
 
